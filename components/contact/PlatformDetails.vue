d729fb96b41efc455ada5570db1d79a9ff262637
Jmr13 <Gamayanjomar@yahoo.com>
2023-06-02 11:50:56 +0800

    Changed style for PlatformDetails component

--- components/contact/PlatformDetails.vue
+++ components/contact/PlatformDetails.vue
@@ -4,23 +4,23 @@
   })
 </script>
 <template>
-  <div>
+  <div class="parentContainer">
     <div v-for="platform in platforms" :key="platform.id" class="container">
       <div>
         <i :class="platform.icon"></i>
       </div>
       <div class="details">
-        <h4>{{ platform.title }}</h4>
+        <h5>{{ platform.title }}</h5>
         <p>{{ platform.details }}</p>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-  div {
+  .parentContainer {
     display: flex;
-    justify-content: space-around;
     flex-wrap: wrap;
+    justify-content: center;
     gap: 2vh 2vw;
   }
   .container {
@@ -31,12 +31,25 @@
     border-radius: var(--rounded);
   }
   i {
-    font-size: 3rem;
+    font-size: 2rem;
+    margin-right: 2vw;
   }
   .details {
     display: flex;
     justify-content: center;
     flex-direction: column;
     flex-grow: 1;
+    gap: 1vh 0;
+  }
+  @media (min-width : 1200px) {
+    i {
+      font-size: 3rem;
+    }
+    h5 {
+      font-size: 2rem;
+    }
+    p {
+      font-size: 2rem;
+    }
   }
 </style>
