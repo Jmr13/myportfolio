966841db400f0fa4bbb6ff0892298cc046af6d6c
Jmr13 <Gamayanjomar@yahoo.com>
2023-08-18 22:18:45 +0800

    Added v-cloak

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,9 +11,14 @@
 <template>
   <div>
     <LoaderLogo v-if="showHideSpinner" />
-    <HeaderLogo />
-    <SettingsThemeSwitcher />
-    <slot />
-    <NavigationBottomBar />
+    <HeaderLogo v-cloak />
+    <SettingsThemeSwitcher v-cloak />
+    <slot v-cloak />
+    <NavigationBottomBar v-cloak />
   </div>
 </template>
+<style scoped>
+  [v-cloak] {
+    display: none
+  }
+</style>
